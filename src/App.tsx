b2ffb4104b49959a280ac41523edd964ea2968e7
YinonTsadik <yinonts1500@gmail.com>
2023-01-16 19:41:36 +0200

    Naming stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import React from 'react'
 import './App.css'
 
-import Navbar from './components/Header/Navbar'
-import Main from './components/Main/Main'
-
 function App() {
-    return (
-        <div className="App">
-            <Navbar />
-            <Main />
-        </div>
-    )
+    return <div className="App"></div>
 }
 
 export default App
